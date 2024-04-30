cfbe0a9cf361f197fe2e58ca0caa66010b58fa00
Shimamura Sakura <shimamura_sakura@pm.me>
2024-04-30 11:20:25 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='iglib',
+    version='1.0.0',
+    description='An assembler / disassembler tool for InnocentGrey \'s script format',
+    long_description='',
+    author='Shimamura Sakura',
+    author_email='shimamura_sakura@pm.me',
+    url='https://github.com/shimamura-sakura/FlowerScript',
+    license='MIT',
+    packages=['iglib'],
+    python_requires='>=3.2',
+)
